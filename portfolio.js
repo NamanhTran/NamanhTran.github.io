5a9d5dc3c8d42ccded9626a6f88a5f1cbebaa0f5
Namanh Tran <NamanhDTran@gmail.com>
2023-01-15 02:42:56 +0000

    actually fixed space width this time

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,11 @@
 function removeConstructionAlert() {
-    let state = alert = document.getElementById("constructionAlert").style.display;
-    if (state == "block")
-        document.getElementById("constructionAlert").style.display = "none";
+  let state = alert = document.getElementById("constructionAlert").style.display;
+  if (state == "block")
+    document.getElementById("constructionAlert").style.display = "none";
 }
 
 function todoAlert() {
-    let alert = document.getElementById("constructionAlert").style.display;
-    if (alert == "none")
-        document.getElementById("constructionAlert").style.display = "block";
+  let alert = document.getElementById("constructionAlert").style.display;
+  if (alert == "none")
+    document.getElementById("constructionAlert").style.display = "block";
 }
